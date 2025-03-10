674c844409922af96299fcc74c3ccccfa2227458
MoloTheSniper <85521815+MoloTheSniper@users.noreply.github.com>
2025-03-10 22:38:23 +0200

    Second Security commit crf tokens

---
+++ src/main/java/com/moloko/SpringSecurityProj/SpringSecurityProjApplication.java
@@ -0,0 +1,13 @@
+package com.moloko.SpringSecurityProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityProjApplication.class, args);
+	}
+
+}
